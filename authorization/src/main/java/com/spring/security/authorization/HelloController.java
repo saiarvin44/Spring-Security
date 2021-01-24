aa2b62d362cd15201e2f5709b8ce1f72746411ae
saiarvin44 <saiarvind44@gmail.com>
2021-01-24 12:08:27 +0530

    chore: applying authorization rules

--- authorization/src/main/java/com/spring/security/authorization/HelloController.java
+++ authorization/src/main/java/com/spring/security/authorization/HelloController.java
@@ -10,4 +10,9 @@ public class HelloController {
   public String hello() {
     return "Hello!";
   }
+
+  @GetMapping("/ciao")
+  public String ciao() {
+    return "ciao";
+  }
 }
--- authorization/src/main/java/com/spring/security/authorization/ProjectConfig.java
+++ authorization/src/main/java/com/spring/security/authorization/ProjectConfig.java
@@ -2,6 +2,7 @@ package com.spring.security.authorization;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -17,16 +18,24 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.httpBasic();
-    http.authorizeRequests().anyRequest().access("hasAuthority('WRITE')");
+    http.authorizeRequests()
+        .mvcMatchers("/hello")
+        .hasRole("ADMIN")
+        .mvcMatchers("/ciao")
+        .hasRole("MANAGER")
+        .mvcMatchers(HttpMethod.GET, "/a")
+        .authenticated()
+        .mvcMatchers(HttpMethod.POST, "/a")
+        .permitAll();
   }
 
   @Bean
   public UserDetailsService userDetailsService() {
     InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
 
-    UserDetails user1 = User.withUsername("john").password("12345").authorities("READ").build();
+    UserDetails user1 = User.withUsername("john").password("12345").roles("ADMIN").build();
 
-    UserDetails user2 = User.withUsername("jane").password("12345").authorities("WRITE").build();
+    UserDetails user2 = User.withUsername("jane").password("12345").authorities("MANAGER").build();
 
     manager.createUser(user1);
     manager.createUser(user2);
